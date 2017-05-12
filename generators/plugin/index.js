a1ba8801c575fb39819ea30ea8262a319c872af6
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-05-12 15:13:51 +0200

    UPDATE add more fields to the plugin json

--- generators/plugin/index.js
+++ generators/plugin/index.js
@@ -1,122 +1,145 @@
 var Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
-  // The name `constructor` is important here
-  constructor(args, opts) {
-    // Calling the super constructor is important so our generator is correctly set up
-    super(args, opts)
-
-    // Next, add your custom code
-    this.option('babel') // This method adds support for a `--babel` flag
-  }
-  
-  prompting() {
-    return this.prompt([{
-      type    : 'input',
-      name    : 'name',
-      message : 'Your plugin name (UpperCamelCase)',
-      default : this.appname
-    },
+    // The name `constructor` is important here
+    constructor(args, opts)
     {
-      type    : 'input',
-      name    : 'author',
-      message : 'The plugin author'
-    },
+        // Calling the super constructor is important so our generator is correctly set up
+        super(args, opts)
+
+        // Next, add your custom code
+        this.option('babel') // This method adds support for a `--babel` flag
+    }
+
+    prompting()
     {
-      type    : 'input',
-      name    : 'license',
-      message : 'The plugin license',
-      default : 'MIT'
-    },
+        return this.prompt([
+                {
+                    type   : 'input',
+                    name   : 'name',
+                    message: 'Your plugin name (UpperCamelCase)',
+                    default: this.appname
+                },
+                {
+                    type   : 'input',
+                    name   : 'author',
+                    message: 'The plugin author'
+                },
+                {
+                    type   : 'input',
+                    name   : 'license',
+                    message: 'The plugin license',
+                    default: 'MIT'
+                },
+                {
+                    type   : 'input',
+                    name   : 'shortDescription',
+                    message: 'Your plugin short description'
+                },
+                {
+                    type   : 'input',
+                    name   : 'price',
+                    message: 'The price of your plugin',
+                    default: 0.00
+                },
+                {
+                    type   : 'input',
+                    name   : 'version',
+                    message: 'Your plugin version',
+                    default: '0.0.1'
+                },
+                {
+                    type   : 'list',
+                    name   : 'categories',
+                    message: 'The plugin type',
+                    choices: [
+                        '3523',
+                        '3518',
+                        'shipping',
+                        'payment',
+                        'general'
+                    ],
+                    default: '3523'
+                },
+                {
+                    type   : 'list',
+                    name   : 'type',
+                    message: 'The plugin type',
+                    choices: [
+                        'general',
+                        'template',
+                        'theme',
+                        'shipping',
+                        'payment'
+                    ],
+                    default: 'general',
+                    store  : true
+                }
+            ]).then((answers) => {
+                this.config.set("answers", answers)
+    })
+        ;
+    }
+
+    writing()
     {
-      type    : 'input',
-      name    : 'shortDescription',
-      message : 'Your plugin short description'
-    },
+
+        let answers = this.config.get("answers")
+
+        this.fs.copyTpl(
+            this.templatePath('plugin.json'),
+            this.destinationPath('plugin.json'),
+            {
+                answers: answers
+            }
+        )
+
+        this.fs.copyTpl(
+            this.templatePath('config.json'),
+            this.destinationPath('config.json')
+        )
+
+        this._copyPhpFiles(answers)
+        this._copyMetaFiles(answers)
+        this._copyTwigFiles(answers)
+    }
+
+    _copyPhpFiles(answers)
     {
-      type    : 'input',
-      name    : 'price',
-      message : 'The price of your plugin',
-      default : 0.00
-    },
+        this.fs.copyTpl(
+            this.templatePath('src/Providers/ServiceProvider.php'),
+            this.destinationPath('src/Providers/' + answers.name + 'ServiceProvider.php'),
+            {name: answers.name}
+        )
+
+        this.fs.copyTpl(
+            this.templatePath('src/Providers/RouteServiceProvider.php'),
+            this.destinationPath('src/Providers/' + answers.name + 'RouteServiceProvider.php'),
+            {name: answers.name}
+        )
+
+        this.fs.copyTpl(
+            this.templatePath('src/Controllers/Controller.php'),
+            this.destinationPath('src/Controllers/' + answers.name + 'Controller.php'),
+            {name: answers.name}
+        )
+    }
+
+    _copyMetaFiles(answers)
     {
-      type    : 'input',
-      name    : 'version',
-      message : 'Your plugin version',
-      default : '0.0.1'
-    },
+        this.fs.copyTpl(
+            this.templatePath('meta'),
+            this.destinationPath('meta'),
+            {name: answers.name, version: answers.version}
+        )
+    }
+
+    _copyTwigFiles(answers)
     {
-      type    : 'list',
-      name    : 'categories',
-      message : 'The plugin type',
-      choices : ['3523', '3518', 'shipping', 'payment', 'general'],
-      default : '3523'
-    },
-    {
-      type    : 'list',
-      name    : 'type',
-      message : 'The plugin type',
-      choices : ['general', 'template', 'theme', 'shipping', 'payment'],
-      default : 'general',
-      store   : true
-    }]).then((answers) => {
-      this.config.set("answers", answers)
-    });
-  }
-
-  writing() {
-    
-    let answers = this.config.get("answers")
-
-    this.fs.copyTpl(
-      this.templatePath('plugin.json'),
-      this.destinationPath('plugin.json'),
-      { name: answers.name,  type: answers.type}
-    )
-
-    this.fs.copyTpl(
-      this.templatePath('config.json'),
-      this.destinationPath('config.json')
-    )
-
-    this._copyPhpFiles(answers)
-    this._copyMetaFiles(answers)
-    this._copyTwigFiles(answers)
-  }
-
-  _copyPhpFiles(answers) {
-    this.fs.copyTpl(
-      this.templatePath('src/Providers/ServiceProvider.php'),
-      this.destinationPath('src/Providers/' + answers.name + 'ServiceProvider.php'),
-      { name: answers.name}
-    )
-
-    this.fs.copyTpl(
-      this.templatePath('src/Providers/RouteServiceProvider.php'),
-      this.destinationPath('src/Providers/' + answers.name + 'RouteServiceProvider.php'),
-      { name: answers.name}
-    )
-
-    this.fs.copyTpl(
-      this.templatePath('src/Controllers/Controller.php'),
-      this.destinationPath('src/Controllers/' + answers.name + 'Controller.php'),
-      { name: answers.name}
-    )
-  }
-
-  _copyMetaFiles(answers) {
-    this.fs.copyTpl(
-      this.templatePath('meta'),
-      this.destinationPath('meta'),
-      { name: answers.name}
-    )
-  }
-
-  _copyTwigFiles(answers) {
-    this.fs.copyTpl(
-      this.templatePath('resources/views/Index.twig'),
-      this.destinationPath('resources/views/Index.twig'),
-      { name: answers.name}
-    )
-  }
+        this.fs.copyTpl(
+            this.templatePath('resources/views/Index.twig'),
+            this.destinationPath('resources/views/Index.twig'),
+            {name: answers.name}
+        )
+    }
 }
