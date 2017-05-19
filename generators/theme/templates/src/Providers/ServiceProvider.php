e8bada6d1e81a67eae757952810ce34f5e2e803b
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-05-19 11:56:38 +0200

    ADD override for homepage to theme generator

--- generators/theme/templates/src/Providers/ServiceProvider.php
+++ generators/theme/templates/src/Providers/ServiceProvider.php
@@ -3,6 +3,8 @@
 namespace <%= name %>\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Log\Services\ReferenceContainer;
+use Plenty\Plugin\Events\Dispatcher;
 
 /**
  * Class <%= name %>ServiceProvider
@@ -10,11 +12,18 @@ use Plenty\Plugin\ServiceProvider;
  */
 class <%= name %>ServiceProvider extends ServiceProvider
 {
-    /**
-    * Register the route service provider
-    */
+    const PRIORITY = 0;
+
     public function register()
     {
         
+    }
+
+    public function boot(Twig $twig, ReferenceContainer $container, Dispatcher $dispatcher)
+    {
+       	// Override template
+        $dispatcher->listen("IO.tpl.home", function (TemplateContainer $container) {
+            $container->setTemplate(<%= name %>::Homepage.Homepage);
+        }, self::PRIORITY);
     }
 }
